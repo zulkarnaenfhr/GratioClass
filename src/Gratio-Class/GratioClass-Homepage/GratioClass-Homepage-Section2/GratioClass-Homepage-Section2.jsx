2e73c68d2532aad41a57eb6a6a5b3e0e230bdfa4
Fahri Izuddin Zulkarnaen <fahrizulkarnaen1@gmail.com>
2021-12-22 11:29:37 +0700

    section 2 selesai

--- src/Gratio-Class/GratioClass-Homepage/GratioClass-Homepage-Section2/GratioClass-Homepage-Section2.jsx
+++ src/Gratio-Class/GratioClass-Homepage/GratioClass-Homepage-Section2/GratioClass-Homepage-Section2.jsx
@@ -6,12 +6,12 @@ export default class GratioClassHomepageSection2 extends Component {
         return (
             <div id="sectionHomepage2">
                 <div className="container">
-                    <div className="row">
+                    <div className="row sectionHomepage-content">
                         <div className="col-md-4">
                             <div id="sectionHomepage2-LeftSide">
                                 <div>
-                                    <h1>halo semuanya</h1>
-                                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe, ut culpa nobis magnam molestias odit.</p>
+                                    <h1 className="sectionHomepage2-title">halo semuanya</h1>
+                                    <p className="sectionHomepage2-penjelasan">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe, ut culpa nobis magnam molestias odit.</p>
                                 </div>
                             </div>
                         </div>
@@ -19,23 +19,35 @@ export default class GratioClassHomepageSection2 extends Component {
                             <div id="sectionHomepage2-RightSide">
                                 <div className="sectionHomepage2-RightSide-Row">
                                     <div className="row">
-                                        <div className="col-md-6">
-                                            <i class="far fa-clock"></i> <h3>lorem</h3>
-                                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex, aliquam.</p>
+                                        <div className="col-md-6 gratioFeatures">
+                                            <div>
+                                                <i class="far fa-clock"></i>
+                                                <h3>lorem</h3>
+                                                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex, aliquam.</p>
+                                            </div>
                                         </div>
-                                        <div className="col-md-6">
-                                            <i class="fas fa-chalkboard-teacher"></i> <h3>lorem</h3>
-                                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex, aliquam.</p>
+                                        <div className="col-md-6 gratioFeatures">
+                                            <div>
+                                                <i class="fas fa-chalkboard-teacher"></i>
+                                                <h3>lorem</h3>
+                                                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex, aliquam.</p>
+                                            </div>
                                         </div>
                                     </div>
                                     <div className="row">
-                                        <div className="col-md-6">
-                                            <i class="fas fa-chart-line"></i> <h3>lorem</h3>
-                                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex, aliquam.</p>
+                                        <div className="col-md-6 gratioFeatures">
+                                            <div>
+                                                <i class="fas fa-chart-line"></i>
+                                                <h3>lorem</h3>
+                                                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex, aliquam.</p>
+                                            </div>
                                         </div>
-                                        <div className="col-md-6">
-                                            <i class="fab fa-connectdevelop"></i> <h3>lorem</h3>
-                                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex, aliquam.</p>
+                                        <div className="col-md-6 gratioFeatures">
+                                            <div>
+                                                <i class="fab fa-connectdevelop"></i>
+                                                <h3>lorem</h3>
+                                                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex, aliquam.</p>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
